49badd33533a804164d6370ee6ae204d049c07ef
anaroshi <anaroshi@naver.com>
2023-03-22 22:20:15 +0900

    회원가입 DB처리(Enum사용)

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -2,11 +2,12 @@ package com.cos.blog.model;
 
 import java.sql.Timestamp;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @Table(name = "user")
 @Entity // User 클래스를 읽어  MySQL에 자동으로 테이블이 생성 한다. 
+// @DynamicInsert // insert 할 때 null인 필드 제외
 public class User {
 
 	@Id // Primary key
@@ -37,8 +39,10 @@ public class User {
 	@Column(nullable = false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role; // Enum을 쓰는게 좋다. admin, user, manager
+	// @ColumnDefault("'user'")
+	// DB는 RoleType이라는 게 없다.
+	@Enumerated(EnumType.STRING)
+	private RoleType role; // Enum을 쓰는게 좋다. ADMIN, USER, MANAGER 값을 강제 시킨다.
 	
 	@CreationTimestamp // 시간 자동 입력
 	private Timestamp createDate;
