b2b124e7c71b9496ffdcf8f939204d80066ac3eb
anaroshi <anaroshi@naver.com>
2023-03-22 23:31:14 +0900

    id로 select 테스트 & 찾는 값이 없을시 exception 람다식 표현

--- src/main/java/com/cos/blog/test/DummyController.java
+++ src/main/java/com/cos/blog/test/DummyController.java
@@ -1,6 +1,8 @@
 package com.cos.blog.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,7 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
-@RestController
+@RestController // html 파일이 아니라 data를 리턴해주는 controller이다.
 public class DummyController {
 	
 	@Autowired // 의존성 주입(DI)
@@ -39,4 +41,32 @@ public class DummyController {
 	 * System.out.println("email : "+email); return "회원가입이 완료되었습니다."; }
 	 */
 	
+	// {id} 주소로 파라메터를 전달 받을 수 있음.
+	// http://localhost:8070/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// user/4을 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 될것 아냐?
+		// 그럼 return null이 리턴되면 프로그램에 문제가 되지
+		// Optional로 너의 User객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return해!!
+		// User user = userRepository.findById(id); // 반환값이 User가 아닌 Optional<User>이다.
+		
+/*
+		   	User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. id : "+id);
+			}
+		}); // 반환값이 User가 아닌 Optional<User>이다.
+*/		
+		// 람다식
+		User user = userRepository.findById(id).orElseThrow(()->{ return new IllegalArgumentException("해당 유저는 없습니다. id : "+id); });
+		
+		// 요청 : 웹브라우저
+		// user 객체 = 자바 오브젝트
+		// 변환 (웹브라우저로 이해할 수 있는 데이터) -> json (Gson라이브러리) => 스프링
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에게 던져준다.
+		return user;
+	}
 }
